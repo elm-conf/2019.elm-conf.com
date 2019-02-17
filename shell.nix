22998dde760d9b86dd79277c1fa27ae6cfa434ce
Brian Hicks <brian@brianthicks.com>
2019-02-17 01:36:37 +0100

    nixify

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import (builtins.fetchTarball rec {
+  # grab a hash from here: https://nixos.org/channels/
+  name = "nixpkgs-darwin-18.09pre153780.9bd45dddf81";
+  url = "https://github.com/nixos/nixpkgs/archive/9bd45dddf8171e2fd4288d684f4f70a2025ded19.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1idrxrymwqfsfysav3yl8lya1jhgg8xzgq9hy7dpdd63770vn8c1";
+}) {};
+
+stdenv.mkDerivation {
+  name = "2019.elm-conf.us";
+  buildInputs = [
+    git
+    elmPackages.elm
+    elmPackages.elm-format
+  ];
+}
