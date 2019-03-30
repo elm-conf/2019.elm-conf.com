ce1ff20ce2e8af1ed1eac59f382deb2d8cf9bf85
Luke Westby <lukewestby1@gmail.com>
2019-03-29 18:07:20 -0700

    npx elm-format

--- script/generate-makefile.py
+++ script/generate-makefile.py
@@ -46,7 +46,7 @@ def generate_html(source):
 ROUTES_RULE = """\
 src/Routes.elm: script/generate-routes.py {sources}
 	$< {source_dests} > $@
-	elm-format --yes $@
+	npx elm-format --yes $@
 """
 
 def routes(sources):
