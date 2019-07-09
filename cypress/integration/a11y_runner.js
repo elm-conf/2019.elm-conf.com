d48926a094babe58cd79963e6096eb80f14a3e97
Brian Hicks <brian@brianthicks.com>
2019-07-09 17:23:22 -0500

    fix missing H1s

--- cypress/integration/a11y_runner.js
+++ cypress/integration/a11y_runner.js
@@ -1,8 +1,14 @@
 describe('Accessibility', function() {
   URLS.split('\n').forEach(function(url) {
     describe(url, function() {
-      it('has not detectable a11y violations on page load', function() {
+      it('has no detectable a11y violations on page load', function() {
+        // set up routing for the markdown file
         cy.visit(url);
+
+        // wait for an H1 to be rendered after Elm gets the markdown content
+        cy.get('h1');
+
+        // do accessibility test
         cy.injectAxe();
         cy.configureAxe({
           rules: [
