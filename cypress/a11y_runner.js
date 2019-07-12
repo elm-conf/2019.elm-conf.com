5f8d2094263b0d40a698cc624ffd6f83ab00e31b
Brian Hicks <brian@brianthicks.com>
2019-07-12 15:51:10 -0500

    add a test for focus/tab behavior

--- cypress/a11y_runner.js
+++ cypress/a11y_runner.js
@@ -1,5 +1,7 @@
 describe('Accessibility', function() {
   URLS.split('\n').forEach(function(url) {
+    if (url.trim() === '') return;
+
     describe(url, function() {
       it('has no detectable a11y violations on page load', function() {
         // set up routing for the markdown file
@@ -11,6 +13,24 @@ describe('Accessibility', function() {
         // do accessibility test
         cy.injectAxe();
         cy.checkA11y();
+      });
+
+      it('has a working skip to content link', function() {
+        // wait for XHR
+        cy.visit(url);
+        cy.get('h1');
+
+        // do tab navigation on the keyboard. The click has to be forced because
+        // of some quirk in Cypress' runner.
+        cy.tab();
+        cy.focused().click({ force: true });
+
+        // assert
+        cy.get('main').then(function(mainQuery) {
+          var main = mainQuery[0];
+          cy.hash().should('eq', '#' + main.id);
+          cy.focused().should('eq', main);
+        });
       });
     });
   });
