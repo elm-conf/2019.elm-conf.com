a43a5339b8b23bc2c481adc7c96b04577ae96802
Dillon Kearns <dillon@dillonkearns.com>
2019-08-22 09:43:14 -0700

    Copy over elm-pages pacakge into local directory.

---
+++ elm-package/src/Pages.elm
@@ -0,0 +1,352 @@
+module Pages exposing (Flags, Parser, Program, application, cliApplication)
+
+import Browser
+import Browser.Navigation
+import Dict exposing (Dict)
+import Head
+import Html exposing (Html)
+import Http
+import Json.Decode
+import Json.Encode
+import Mark
+import Pages.Content as Content exposing (Content)
+import Pages.ContentCache as ContentCache exposing (ContentCache)
+import Pages.Document
+import Pages.Manifest as Manifest
+import Pages.Parser exposing (Page)
+import Result.Extra
+import Task exposing (Task)
+import Url exposing (Url)
+
+
+type alias Content =
+    List ( List String, { extension : String, frontMatter : String, body : Maybe String } )
+
+
+type alias Program userModel userMsg metadata view =
+    Platform.Program Flags (Model userModel userMsg metadata view) (Msg userMsg metadata view)
+
+
+mainView :
+    (userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg })
+    -> ModelDetails userModel userMsg metadata view
+    -> { title : String, body : Html userMsg }
+mainView pageView model =
+    case model.contentCache of
+        Ok site ->
+            pageViewOrError pageView model (Ok site)
+
+        -- TODO these lookup helpers should not need it to be a Result
+        Err errorView ->
+            { title = "Error parsing"
+            , body = errorView
+            }
+
+
+pageViewOrError :
+    (userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg })
+    -> ModelDetails userModel userMsg metadata view
+    -> ContentCache userMsg metadata view
+    -> { title : String, body : Html userMsg }
+pageViewOrError pageView model cache =
+    case ContentCache.lookup cache model.url of
+        Just entry ->
+            case entry of
+                ContentCache.Parsed metadata viewList ->
+                    pageView model.userModel
+                        (Result.map ContentCache.extractMetadata cache |> Result.withDefault []
+                         -- TODO handle error better
+                        )
+                        { metadata = metadata
+                        , view = viewList
+                        }
+
+                ContentCache.NeedContent extension _ ->
+                    { title = "", body = Html.text "" }
+
+                ContentCache.Unparsed extension _ _ ->
+                    { title = "", body = Html.text "" }
+
+        Nothing ->
+            { title = "Page not found"
+            , body =
+                Html.div []
+                    [ Html.text "Page not found. Valid routes:\n\n"
+                    , cache
+                        |> ContentCache.routesForCache
+                        |> String.join ", "
+                        |> Html.text
+                    ]
+            }
+
+
+view :
+    Content
+    -> (userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg })
+    -> ModelDetails userModel userMsg metadata view
+    -> Browser.Document (Msg userMsg metadata view)
+view content pageView model =
+    let
+        { title, body } =
+            mainView pageView model
+    in
+    { title = title
+    , body =
+        [ body
+            |> Html.map UserMsg
+        ]
+    }
+
+
+encodeHeads : List Head.Tag -> Json.Encode.Value
+encodeHeads head =
+    Json.Encode.list Head.toJson head
+
+
+type alias Flags =
+    { imageAssets : Json.Decode.Value
+    }
+
+
+combineTupleResults :
+    List ( List String, Result error success )
+    -> Result error (List ( List String, success ))
+combineTupleResults input =
+    input
+        |> List.map
+            (\( path, result ) ->
+                result
+                    |> Result.map (\success -> ( path, success ))
+            )
+        |> Result.Extra.combine
+
+
+init :
+    Pages.Document.Document metadata view
+    -> (Json.Encode.Value -> Cmd (Msg userMsg metadata view))
+    -> (metadata -> List Head.Tag)
+    -> Content
+    -> ( userModel, Cmd userMsg )
+    -> Flags
+    -> Url
+    -> Browser.Navigation.Key
+    -> ( ModelDetails userModel userMsg metadata view, Cmd (Msg userMsg metadata view) )
+init document toJsPort head content initUserModel flags url key =
+    let
+        ( userModel, userCmd ) =
+            initUserModel
+
+        imageAssets =
+            Json.Decode.decodeValue
+                (Json.Decode.dict Json.Decode.string)
+                flags.imageAssets
+                |> Result.withDefault Dict.empty
+
+        -- metadata =
+        --     Pages.Document.parseMetadata document
+        --         content
+        --         |> combineTupleResults
+        contentCache =
+            ContentCache.init document content
+    in
+    case contentCache of
+        Ok okCache ->
+            ( { key = key
+              , url = url
+              , imageAssets = imageAssets
+              , userModel = userModel
+              , contentCache = contentCache
+              }
+            , Cmd.batch
+                ([ Content.lookup (ContentCache.extractMetadata okCache) url
+                    |> Maybe.map head
+                    |> Maybe.map encodeHeads
+                    |> Maybe.map toJsPort
+                 , userCmd |> Cmd.map UserMsg |> Just
+                 , contentCache
+                    |> ContentCache.lazyLoad document url
+                    |> Task.attempt UpdateCache
+                    |> Just
+                 ]
+                    |> List.filterMap identity
+                )
+            )
+
+        Err _ ->
+            ( { key = key
+              , url = url
+              , imageAssets = imageAssets
+              , userModel = userModel
+              , contentCache = Ok Dict.empty -- TODO use ContentCache.init
+              }
+            , Cmd.batch
+                [ userCmd |> Cmd.map UserMsg
+                ]
+              -- TODO handle errors better
+            )
+
+
+type Msg userMsg metadata view
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | UserMsg userMsg
+    | UpdateCache (Result Http.Error (ContentCache userMsg metadata view))
+    | UpdateCacheAndUrl Url (Result Http.Error (ContentCache userMsg metadata view))
+
+
+type Model userModel userMsg metadata view
+    = Model (ModelDetails userModel userMsg metadata view)
+    | CliModel
+
+
+type alias ModelDetails userModel userMsg metadata view =
+    { key : Browser.Navigation.Key
+    , url : Url.Url
+    , imageAssets : Dict String String
+    , contentCache : ContentCache userMsg metadata view
+    , userModel : userModel
+    }
+
+
+update :
+    Pages.Document.Document metadata view
+    -> (userMsg -> userModel -> ( userModel, Cmd userMsg ))
+    -> Msg userMsg metadata view
+    -> ModelDetails userModel userMsg metadata view
+    -> ( ModelDetails userModel userMsg metadata view, Cmd (Msg userMsg metadata view) )
+update document userUpdate msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    let
+                        navigatingToSamePage =
+                            url.path == model.url.path
+                    in
+                    if navigatingToSamePage then
+                        -- this is a workaround for an issue with anchor fragment navigation
+                        -- see https://github.com/elm/browser/issues/39
+                        ( model, Browser.Navigation.load (Url.toString url) )
+
+                    else
+                        ( model, Browser.Navigation.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Browser.Navigation.load href )
+
+        UrlChanged url ->
+            ( model
+            , model.contentCache
+                |> ContentCache.lazyLoad document url
+                |> Task.attempt (UpdateCacheAndUrl url)
+            )
+
+        UserMsg userMsg ->
+            let
+                ( userModel, userCmd ) =
+                    userUpdate userMsg model.userModel
+            in
+            ( { model | userModel = userModel }, userCmd |> Cmd.map UserMsg )
+
+        UpdateCache cacheUpdateResult ->
+            case cacheUpdateResult of
+                -- TODO can there be race conditions here? Might need to set something in the model
+                -- to keep track of the last url change
+                Ok updatedCache ->
+                    ( { model | contentCache = updatedCache }, Cmd.none )
+
+                Err _ ->
+                    -- TODO handle error
+                    ( model, Cmd.none )
+
+        UpdateCacheAndUrl url cacheUpdateResult ->
+            case cacheUpdateResult of
+                -- TODO can there be race conditions here? Might need to set something in the model
+                -- to keep track of the last url change
+                Ok updatedCache ->
+                    ( { model | url = url, contentCache = updatedCache }, Cmd.none )
+
+                Err _ ->
+                    -- TODO handle error
+                    ( { model | url = url }, Cmd.none )
+
+
+type alias Parser metadata view =
+    Dict String String
+    -> List String
+    -> List ( List String, metadata )
+    -> Mark.Document view
+
+
+application :
+    { init : ( userModel, Cmd userMsg )
+    , update : userMsg -> userModel -> ( userModel, Cmd userMsg )
+    , subscriptions : userModel -> Sub userMsg
+    , view : userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg }
+    , document : Pages.Document.Document metadata view
+    , content : Content
+    , toJsPort : Json.Encode.Value -> Cmd (Msg userMsg metadata view)
+    , head : metadata -> List Head.Tag
+    , manifest : Manifest.Config
+    }
+    -> Program userModel userMsg metadata view
+application config =
+    Browser.application
+        { init =
+            \flags url key ->
+                init config.document config.toJsPort config.head config.content config.init flags url key
+                    |> Tuple.mapFirst Model
+        , view =
+            \outerModel ->
+                case outerModel of
+                    Model model ->
+                        view config.content config.view model
+
+                    CliModel ->
+                        { title = "Error"
+                        , body = [ Html.text "Unexpected state" ]
+                        }
+        , update =
+            \msg outerModel ->
+                case outerModel of
+                    Model model ->
+                        update config.document config.update msg model |> Tuple.mapFirst Model
+
+                    CliModel ->
+                        ( outerModel, Cmd.none )
+        , subscriptions =
+            \outerModel ->
+                case outerModel of
+                    Model model ->
+                        config.subscriptions model.userModel
+                            |> Sub.map UserMsg
+
+                    CliModel ->
+                        Sub.none
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+cliApplication :
+    { init : ( userModel, Cmd userMsg )
+    , update : userMsg -> userModel -> ( userModel, Cmd userMsg )
+    , subscriptions : userModel -> Sub userMsg
+    , view : userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg }
+    , document : Pages.Document.Document metadata view
+    , content : Content
+    , toJsPort : Json.Encode.Value -> Cmd (Msg userMsg metadata view)
+    , head : metadata -> List Head.Tag
+    , manifest : Manifest.Config
+    }
+    -> Program userModel userMsg metadata view
+cliApplication config =
+    Platform.worker
+        { init =
+            \flags ->
+                ( CliModel
+                , config.toJsPort (Manifest.toJson config.manifest)
+                )
+        , update = \msg model -> ( model, Cmd.none )
+        , subscriptions = \_ -> Sub.none
+        }
---
+++ elm-package/src/Pages/ContentCache.elm
@@ -0,0 +1,287 @@
+module Pages.ContentCache exposing (ContentCache, Entry(..), Page, Path, extractMetadata, init, lazyLoad, lookup, pathForUrl, routesForCache, update)
+
+import Dict exposing (Dict)
+import Html exposing (Html)
+import Http
+import Json.Decode
+import Mark
+import Mark.Error
+import Pages.Document
+import Result.Extra
+import Task exposing (Task)
+import Url exposing (Url)
+import Url.Builder
+
+
+type alias Content =
+    -- { markdown : List ( List String, { frontMatter : String, body : Maybe String } )
+    -- , markup :
+    --     List ( List String, { frontMatter : String, body : Maybe String } )
+    -- }
+    List ( List String, { extension : String, frontMatter : String, body : Maybe String } )
+
+
+type alias ContentCache msg metadata view =
+    Result (Html msg) (Dict Path (Entry metadata view))
+
+
+type alias ContentCacheInner metadata view =
+    Dict Path (Entry metadata view)
+
+
+type Entry metadata view
+    = NeedContent String metadata
+    | Unparsed String metadata String
+      -- TODO need to have an UnparsedMarkup entry type so the right parser is applied
+    | Parsed metadata view
+
+
+type alias Path =
+    List String
+
+
+extractMetadata : ContentCacheInner metadata view -> List ( Path, metadata )
+extractMetadata cache =
+    cache
+        |> Dict.toList
+        |> List.map (\( path, entry ) -> ( path, getMetadata entry ))
+
+
+getMetadata : Entry metadata view -> metadata
+getMetadata entry =
+    case entry of
+        NeedContent extension metadata ->
+            metadata
+
+        Unparsed extension metadata _ ->
+            metadata
+
+        Parsed metadata _ ->
+            metadata
+
+
+init :
+    Pages.Document.Document metadata view
+    -> Content
+    -> ContentCache msg metadata view
+init document content =
+    Pages.Document.parseMetadata document content
+        |> List.map
+            (Tuple.mapSecond
+                (Result.map
+                    (\{ metadata, extension } -> NeedContent extension metadata)
+                )
+            )
+        |> combineTupleResults
+        |> Result.mapError (\_ -> Html.text "Error")
+        |> Result.map Dict.fromList
+
+
+routes : List ( List String, anything ) -> List String
+routes record =
+    record
+        |> List.map Tuple.first
+        |> List.map (String.join "/")
+        |> List.map (\route -> "/" ++ route)
+
+
+routesForCache : ContentCache msg metadata view -> List String
+routesForCache cacheResult =
+    case cacheResult of
+        Ok cache ->
+            cache
+                |> Dict.toList
+                |> routes
+
+        Err _ ->
+            []
+
+
+type alias Page metadata view =
+    { metadata : metadata
+    , view : view
+    }
+
+
+renderErrors : ( List String, List Mark.Error.Error ) -> Html msg
+renderErrors ( path, errors ) =
+    Html.div []
+        [ Html.text (path |> String.join "/")
+        , errors
+            |> List.map (Mark.Error.toHtml Mark.Error.Light)
+            |> Html.div []
+        ]
+
+
+combineTupleResults :
+    List ( List String, Result error success )
+    -> Result error (List ( List String, success ))
+combineTupleResults input =
+    input
+        |> List.map
+            (\( path, result ) ->
+                result
+                    |> Result.map (\success -> ( path, success ))
+            )
+        |> Result.Extra.combine
+
+
+{-| Get from the Cache... if it's not already parsed, it will
+parse it before returning it and store the parsed version in the Cache
+-}
+lazyLoad :
+    Pages.Document.Document metadata view
+    -> Url
+    -> ContentCache msg metadata view
+    -> Task Http.Error (ContentCache msg metadata view)
+lazyLoad document url cacheResult =
+    case cacheResult of
+        Err _ ->
+            Task.succeed cacheResult
+
+        Ok cache ->
+            case Dict.get (pathForUrl url) cache of
+                Just entry ->
+                    case entry of
+                        NeedContent extension _ ->
+                            httpTask url
+                                |> Task.map
+                                    (\downloadedContent ->
+                                        -- |> Result.mapError (\_ -> Html.text "")
+                                        update cacheResult
+                                            (\thing ->
+                                                Pages.Document.parseContent extension thing document
+                                                    |> Result.mapError (\_ -> Html.text "TODO")
+                                            )
+                                            url
+                                            downloadedContent
+                                    )
+
+                        Unparsed extension metadata content ->
+                            update cacheResult
+                                (\thing ->
+                                    Pages.Document.parseContent extension thing document
+                                        |> Result.mapError (\_ -> Html.text "TODO")
+                                )
+                                url
+                                content
+                                |> Task.succeed
+
+                        Parsed _ _ ->
+                            Task.succeed cacheResult
+
+                Nothing ->
+                    Task.succeed cacheResult
+
+
+
+-- renderMarkup : String -> view
+-- ren
+
+
+httpTask url =
+    Http.task
+        { method = "GET"
+        , headers = []
+        , url =
+            Url.Builder.absolute
+                ((url.path |> String.split "/" |> List.filter (not << String.isEmpty))
+                    ++ [ "content.txt"
+                       ]
+                )
+                []
+        , body = Http.emptyBody
+        , resolver =
+            Http.stringResolver
+                (\response ->
+                    case response of
+                        Http.BadUrl_ url_ ->
+                            Err (Http.BadUrl url_)
+
+                        Http.Timeout_ ->
+                            Err Http.Timeout
+
+                        Http.NetworkError_ ->
+                            Err Http.NetworkError
+
+                        Http.BadStatus_ metadata body ->
+                            Err (Http.BadStatus metadata.statusCode)
+
+                        Http.GoodStatus_ metadata body ->
+                            Ok body
+                )
+        , timeout = Nothing
+        }
+
+
+update :
+    ContentCache msg metadata view
+    -> (String -> Result (Html msg) view)
+    -> Url
+    -> String
+    -> ContentCache msg metadata view
+update cacheResult renderer url rawContent =
+    case cacheResult of
+        Ok cache ->
+            Dict.update (pathForUrl url)
+                (\entry ->
+                    case entry of
+                        Just (Parsed metadata view) ->
+                            entry
+
+                        Just (Unparsed extension metadata content) ->
+                            case renderer content of
+                                Ok value ->
+                                    Just (Parsed metadata value)
+
+                                Err _ ->
+                                    Nothing
+
+                        Just (NeedContent extension metadata) ->
+                            case renderer rawContent of
+                                Ok value ->
+                                    Just (Parsed metadata value)
+
+                                Err _ ->
+                                    Nothing
+
+                        Nothing ->
+                            -- TODO this should never happen
+                            Nothing
+                )
+                cache
+                |> Ok
+
+        Err error ->
+            -- TODO update this ever???
+            -- Should this be something other than the raw HTML, or just concat the error HTML?
+            Err error
+
+
+pathForUrl : Url -> Path
+pathForUrl url =
+    url.path
+        |> dropTrailingSlash
+        |> String.split "/"
+        |> List.drop 1
+
+
+lookup :
+    ContentCache msg metadata view
+    -> Url
+    -> Maybe (Entry metadata view)
+lookup content url =
+    case content of
+        Ok dict ->
+            Dict.get (pathForUrl url) dict
+
+        Err _ ->
+            Nothing
+
+
+dropTrailingSlash path =
+    if path |> String.endsWith "/" then
+        String.dropRight 1 path
+
+    else
+        path
